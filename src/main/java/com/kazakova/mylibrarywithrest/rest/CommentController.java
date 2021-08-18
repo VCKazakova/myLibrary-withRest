652e0861bafcb1d124c7ee7fbce85354f23569b6
VCKazakova <v.c.kazakova@gmail.com>
2021-08-18 14:40:22 +0300

    master update logging for CommentController

--- src/main/java/com/kazakova/mylibrarywithrest/rest/CommentController.java
+++ src/main/java/com/kazakova/mylibrarywithrest/rest/CommentController.java
@@ -3,12 +3,14 @@ package com.kazakova.mylibrarywithrest.rest;
 import com.kazakova.mylibrarywithrest.domain.Comment;
 import com.kazakova.mylibrarywithrest.dto.CommentDto;
 import com.kazakova.mylibrarywithrest.service.CommentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Slf4j
 public class CommentController {
 
     private final CommentService service;
@@ -23,9 +25,12 @@ public class CommentController {
             method = RequestMethod.GET
     )
     public List<CommentDto> get() {
-        return service.findAllComments().stream()
+        log.info(">> CommentController getAllComments");
+        List<CommentDto> allComments = service.findAllComments().stream()
                 .map(CommentDto::toDto)
                 .collect(Collectors.toList());
+        log.info(">> CommentController getAllComments allComments={}", allComments);
+        return allComments;
     }
 
     @RequestMapping(
@@ -35,19 +40,26 @@ public class CommentController {
     public CommentDto get(
             @PathVariable("id") Long id
     ) {
+        log.info(">> CommentController getCommentById id={}", id);
         Comment comment = service.findCommentById(id).get();
-        return CommentDto.toDto(comment);
+        CommentDto commentById = CommentDto.toDto(comment);
+        log.info(">> CommentController getCommentById commentById={}", commentById);
+        return commentById;
     }
 
     @PostMapping("/createComment")
     public CommentDto createComment(@RequestBody CommentDto dto) {
+        log.info(">> CommentController createComment dto={}", dto);
         Comment comment = dto.toDomainObject();
         Comment newComment = service.createComment(comment);
-        return CommentDto.toDto(newComment);
+        CommentDto createdComment = CommentDto.toDto(newComment);
+        log.info(">> CommentController createComment createdComment={}", createdComment);
+        return createdComment;
     }
 
     @DeleteMapping("/comment/{id}")
     public void delete(@PathVariable("id") Long id) {
+        log.info(">> CommentController deleteComment id={}", id);
         service.deleteCommentById(id);
     }
 
@@ -56,9 +68,11 @@ public class CommentController {
             @PathVariable("id") Long id,
             @RequestParam("comment") String comment
     ) {
+        log.info(">> CommentController changeComment id={}", id);
         Comment commentForChange = service.findCommentById(id).get();
         commentForChange.setComment(comment);
-        service.createComment(commentForChange);
+        Comment changedComment = service.createComment(commentForChange);
+        log.info(">> CommentController changeComment changedComment={}", changedComment);
     }
 
 }
