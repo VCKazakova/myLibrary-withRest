b4479d43fecfc2f555c45a6b4f30ca939e83c082
VCKazakova <v.c.kazakova@gmail.com>
2021-08-18 14:50:17 +0300

    master update logging for GenreController

--- src/main/java/com/kazakova/mylibrarywithrest/rest/GenreController.java
+++ src/main/java/com/kazakova/mylibrarywithrest/rest/GenreController.java
@@ -5,12 +5,14 @@ import com.kazakova.mylibrarywithrest.domain.Genre;
 import com.kazakova.mylibrarywithrest.dto.AuthorDto;
 import com.kazakova.mylibrarywithrest.dto.GenreDto;
 import com.kazakova.mylibrarywithrest.service.GenreService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Slf4j
 public class GenreController {
 
     private final GenreService service;
@@ -24,9 +26,12 @@ public class GenreController {
             method = RequestMethod.GET
     )
     public List<GenreDto> get() {
-        return service.findAllGenres().stream()
+        log.info(">> GenreController getAllGenres");
+        List<GenreDto> allGenres = service.findAllGenres().stream()
                 .map(GenreDto::toDto)
                 .collect(Collectors.toList());
+        log.info(">> GenreController getAllGenres allGenres={}", allGenres);
+        return allGenres;
     }
 
     @RequestMapping(
@@ -36,19 +41,26 @@ public class GenreController {
     public GenreDto get(
             @PathVariable("id") Long id
     ) {
+        log.info(">> GenreController getGenreById id={}", id);
         Genre genre = service.findGenreById(id).get();
-        return GenreDto.toDto(genre);
+        GenreDto genreById = GenreDto.toDto(genre);
+        log.info(">> GenreController getGenreById genreById={}", genreById);
+        return genreById;
     }
 
     @PostMapping("/genre")
     public GenreDto create(GenreDto dto) {
+        log.info(">> GenreController createGenre dto={}", dto);
         Genre genre = dto.toDomainObject();
         Genre newGenre = service.createGenre(genre);
-        return GenreDto.toDto(newGenre);
+        GenreDto createdGenre = GenreDto.toDto(newGenre);
+        log.info(">> GenreController createGenre createdGenre={}", createdGenre);
+        return createdGenre;
     }
 
     @DeleteMapping("/genre/{id}")
     public void delete(@PathVariable("id") Long id) {
+        log.info(">> GenreController deleteGenre id={}", id);
         service.deleteGenreById(id);
     }
 
@@ -57,9 +69,11 @@ public class GenreController {
             @PathVariable("id") Long id,
             @RequestParam("name") String name
     ) {
+        log.info(">> GenreController changeGenre id={}", id);
         Genre genre = service.findGenreById(id).get();
         genre.setName(name);
-        service.createGenre(genre);
+        Genre changedGenre = service.createGenre(genre);
+        log.info(">> GenreController changeGenre changedGenre={}", changedGenre);
     }
 
 }
