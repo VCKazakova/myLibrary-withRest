cafb2262ab3b68364a2af2323ded64002e6d2a0c
VCKazakova <v.c.kazakova@gmail.com>
2021-08-20 09:51:33 +0300

    master create files for testing

---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,43 @@
+INSERT INTO authors
+VALUES (DEFAULT, 'Green'),
+       (DEFAULT, 'King'),
+       (DEFAULT, 'Pushkin'),
+       (DEFAULT, 'Lermontov'),
+       (DEFAULT, 'Gogol'),
+       (DEFAULT, 'Shekspir'),
+       (DEFAULT, 'Esenin'),
+       (DEFAULT, 'Bulgakov');
+
+INSERT INTO genres
+VALUES (DEFAULT, 'Roman'),
+       (DEFAULT, 'Comedy'),
+       (DEFAULT, 'Tale'),
+       (DEFAULT, 'Tragedy'),
+       (DEFAULT, 'Legend'),
+       (DEFAULT, 'Myth');
+
+INSERT INTO books
+VALUES (DEFAULT, 'Red sails', 1, 1),
+       (DEFAULT, 'Kerry', 2, 1),
+       (DEFAULT, 'Mtciri', 4, 1),
+       (DEFAULT, 'Evgeni Onegin', 3, 1),
+       (DEFAULT, 'Cemetery of pets', 2, 1),
+       (DEFAULT, 'Gold cock', 3, 3),
+       (DEFAULT, 'The dead souls', 5, 2),
+       (DEFAULT, 'Romeo and Juliet', 6, 4),
+       (DEFAULT, 'Anna Snegina', 6, 1),
+       (DEFAULT, 'The heart of a dog', 6, 1);
+
+INSERT INTO comments
+VALUES (DEFAULT, 'Brrr', 2),
+       (DEFAULT, 'Wow', 1),
+       (DEFAULT, 'Omg', 2),
+       (DEFAULT, 'Super', 5),
+       (DEFAULT, 'Dislike', 4),
+       (DEFAULT, 'Sad', 3),
+       (DEFAULT, 'Ou my god', 7),
+       (DEFAULT, 'Pam-pam', 8),
+       (DEFAULT, 'I like it', 9),
+       (DEFAULT, 'I want to read again', 10);
+
+
