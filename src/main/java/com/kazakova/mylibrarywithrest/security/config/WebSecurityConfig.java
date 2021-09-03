fa3daf8ebfddfd1ea4325cb910de73ddf2b28185
VCKazakova <v.c.kazakova@gmail.com>
2021-09-03 08:51:51 +0300

    master create WebSecurityConfig

---
+++ src/main/java/com/kazakova/mylibrarywithrest/security/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.kazakova.mylibrarywithrest.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    private TokenAuthFilter tokenAuthFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .addFilterBefore(tokenAuthFilter, BasicAuthenticationFilter.class)
+                .antMatcher("/**")
+                .authenticationProvider(authenticationProvider)
+                .authorizeRequests()
+                .antMatchers("/users/**").hasAuthority("USER")
+                .antMatchers("/login").permitAll();
+        http.csrf().disable();
+    }
+}
