ae0e6fa472a683a76a958258169d33524a7b4543
VCKazakova <v.c.kazakova@gmail.com>
2021-09-03 08:03:58 +0300

    master create entity User

---
+++ src/main/java/com/kazakova/mylibrarywithrest/domain/User.java
@@ -0,0 +1,35 @@
+package com.kazakova.mylibrarywithrest.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "rest_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String hashPassword;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private State state;
+
+    @OneToMany(mappedBy = "user")
+    List<Token> tokens;
+
+
+}
